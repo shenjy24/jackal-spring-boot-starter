f78473e5c2e2aca3bb3e36fa65e71ab09905028d
shenjy <shenjiayun@hzyushi.cn>
2018-10-17 10:06:36 +0800

    guavaxianliu

--- src/main/java/com/shenjy/controller/UserController.java
+++ src/main/java/com/shenjy/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.shenjy.controller;
 
+import com.google.common.util.concurrent.RateLimiter;
 import com.shenjy.common.JsonResult;
+import com.shenjy.common.SystemCode;
 import com.shenjy.entities.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +18,8 @@ import java.util.Date;
 @RequestMapping("/user")
 public class UserController extends BaseController{
 
+    private RateLimiter rateLimiter = RateLimiter.create(10);
+
     @RequestMapping("/saveUser")
     public JsonResult hello() {
         User user = new User();
@@ -24,4 +28,21 @@ public class UserController extends BaseController{
         user.setCtime(new Date());
         return success(user);
     }
+
+    /**
+     * 使用guava类库的RateLimiter进行限流
+     */
+    @RequestMapping("/getUser")
+    public JsonResult getUser() {
+        if (!rateLimiter.tryAcquire()) {
+            return error(SystemCode.HANDLE_EXCEPTION);
+        }
+
+        User user = new User();
+        user.setUid(1L);
+        user.setName("Tom");
+        user.setCtime(new Date());
+        return success(user);
+    }
+
 }
